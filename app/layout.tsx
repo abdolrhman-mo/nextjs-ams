fe05a97629e746d2b16c5378005bdeb95d8767e1
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2024-08-19 16:40:45 +0300

    commited

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-neutral-100 text-gray-500 pt-20">
+        {children}
+      </body>
     </html>
   );
 }
