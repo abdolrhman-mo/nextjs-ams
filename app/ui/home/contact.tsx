8f236b1410d7a9886cdceb4763428f3fa690498f
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2024-09-09 10:52:30 +0300

    added makadysolutions@gmail.com

--- app/ui/home/contact.tsx
+++ app/ui/home/contact.tsx
@@ -10,9 +10,9 @@ export default function Contact() {
                 <div className="flex justify-between md:grid grid-cols-3 ">
                     <div className="font-semibold capitalize col-span-1">mail</div>
                     <div className="col-span-2">
-                        <a className="hover:underline" href="mailto:abdelrhmanlearn@gmail.com" target="_blank">
+                        <a className="hover:underline" href="mailto:makadysolutions@gmail.com" target="_blank">
                           {/* <svg className="pr-1.5 h-3.5 hidden md:inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg> */}
-                          abdelrhmanlearn@gmail.com
+                          makadysolutions@gmail.com
                         </a>
                     </div>
                 </div>
